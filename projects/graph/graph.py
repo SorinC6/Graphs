d54b7bddce34d0950f33dc6983e77222856f4e57
Sorin Chis <kish.sorin@yahoo.com>
2019-11-04 14:43:33 +0200

    initial commit

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,39 +3,81 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Cannot create edge based on given vertices")
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # create a Queue
+        queue = Queue()
+
+        # create a list of visited nodes
+        visited = set()
+        # put the started node in queue
+        queue.enqueue(starting_vertex)
+        while queue.size() > 0:
+            # pop the first node
+            vertex = queue.dequeue()
+            # if not visited
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                # get adjacent adges and add to the list
+                for next_vert in self.vertices[vertex]:
+                    queue.enqueue(next_vert)
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # create a Stack
+        stack = Stack()
+
+        # create a list of visited nodes
+        visited = set()
+        # put the started node in queue
+        stack.push(starting_vertex)
+        while stack.size() > 0:
+            # pop the first node
+            vertex = stack.pop()
+            # if not visited
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                # get adjacent adges and add to the list
+                for next_vert in self.vertices[vertex]:
+                    stack.push(next_vert)
+
     def dft_recursive(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         This should be done using recursion.
         """
-        pass  # TODO
+
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
@@ -43,6 +85,7 @@ class Graph:
         breath-first order.
         """
         pass  # TODO
+
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
@@ -50,9 +93,6 @@ class Graph:
         depth-first order.
         """
         pass  # TODO
-
-
-
 
 
 if __name__ == '__main__':
